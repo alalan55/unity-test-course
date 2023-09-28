3d2263efdaaaa80f66433c3dcae68ae4095e2a37
alalan55 <alla.ao4975@gmail.com>
2023-09-28 11:14:58 -0300

    Aula 4

--- src/components/HelloWorld.spec.js
+++ src/components/HelloWorld.spec.js
@@ -1,9 +1,32 @@
-import { it } from "vitest";
+import { it, expect } from "vitest";
 import { mount } from "@vue/test-utils";
 import HelloWorld from "./HelloWorld.vue";
 
 it("Deve renderizar a prop msg", () => {
+  const wrapper = mount(HelloWorld, {
+    props: {
+      msg: "Primeiro teste",
+    },
+  });
+
+  expect(wrapper.find("h1").text()).toBe("Primeiro teste");
+});
+
+it("Deve adicionar valor no contador quandoo método é chamado (withebox testing)", () => {
+  // whitebox testing
   const wrapper = mount(HelloWorld);
 
-  console.log(wrapper.html())
+  wrapper.vm.increment();
+
+  expect(wrapper.vm.count).toBe(1);
+});
+
+it("Deve adicionar valor no contador e mostrar o valor atualizado (blackbox testing)", async () => {
+  const wrapper = mount(HelloWorld);
+
+  const button = wrapper.find("button");
+
+  await button.trigger("click");
+
+  expect(button.text()).toBe("count is 1");
 });
